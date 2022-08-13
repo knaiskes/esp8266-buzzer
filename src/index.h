b56d8d0a6fd4b6b46b5db878262483af8a667315
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2022-08-13 23:45:28 +0300

    Add index.h

---
+++ src/index.h
@@ -0,0 +1,10 @@
+const char HTML_PAGE[] =
+"<!DOCTYPE html>"
+"<html>"
+"<head>"
+"<meta name=\"viewport\" content=\"width=device-width, inital-scale=1.0\">"
+"<title>Activate buzzer</title>"
+"</head>"
+"<h4>Refresh to activate buzzer</h4>"
+"</body>"
+"</html>";
