6250a8f60127c773c031cd51132c6ad09bd19d37
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2022-08-14 01:12:24 +0300

    Add functionallity to connect to WiFi and start a Web server

--- src/buzzer.ino
+++ src/buzzer.ino
@@ -1,13 +1,41 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <WiFiClient.h>
+
+#include "index.h"
+
+// network
+const char* ssid = "";
+const char* password = "";
+const char* hostName = "buzzer";
+
+const int port = 80;
+
 const int buzzerPin = 14; // D5
 const int frequency = 1400;
 const int duration = 5000;
 
+ESP8266WebServer server(port);
+
+void handleIndex() {
+  tone(buzzerPin, frequency, duration);
+  server.send(200, "text/html", INDEX_PAGE);
+}
+
+
 void setup() {
-  pinMode(buzzerPin, OUTPUT);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) { delay(500); }
+
+  WiFi.hostname(hostName);
+
+  server.on("/", handleIndex);
+  server.begin();
 }
 
 void loop()
 {
-  tone(buzzerPin, frequency, duration);
-  delay(1000); // 1 second
+  server.handleClient();
 }
